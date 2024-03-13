b90a3402661098fb0d12ff206b676d96be6e359f
xibo <xibo@portkey.finance>
2024-03-13 23:21:53 +0800

    feat/add-mongo-store

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,25 +1,30 @@
 import pymongo
-from .constants import *
 
-def get_db(db_name):
+class DataEntry:
+    def __init__(self, description, submitter, create_date, status, imageResults):
+        self.description = description
+        self.submitter = submitter
+        self.create_date = create_date
+        self.status = status
+        self.result_link = imageResults
+
+def get_db():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    return client[db_name]
+    db = client["experimentPlatform"]
+    return db
 
+def get_collection(db, collection_name):
+    _collection = db[collection_name]
+    return _collection
 
-def get_collection(db_name):
-    db = get_db(db_name)
-    return db[COLLECTION_NAME]
+def insert(collection, data):
+    res = collection.insert_one(data)
+    print(res)
+    _id = res.inserted_id
+    print(_id)
 
-
-def create_collection_if_not_exists(db_name):
-    db = get_db(db_name)
-
-    if COLLECTION_NAME not in db.list_collection_names():
-        db.create_collection(COLLECTION_NAME)
-
-    # Access the collection
-    coll = db[COLLECTION_NAME]
-
-    # Step 4: Create Index
-    # This creates an index on the 'prompt' field
-    coll.create_index([('prompt', 1)])  # 1 for ascending order
+def query(collection):
+    res = []
+    for x in collection.find({}, {"submitter": "xibo"}):
+        res.append(x)
+    print(res)
